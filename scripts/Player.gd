75708df867928eebba96847264012fc01d3e83db
djalexkidd <alexpyr@outlook.fr>
2022-02-19 13:40:36 +0100

    Pioup!

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,7 +5,6 @@ const SPEED = 6
 const ROTSPEED = 10
 const GRAVITY = 0.2
 const JUMPFORCE = 8
-var debug_mode
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
@@ -21,6 +20,7 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMPFORCE
+		$JumpSound.play()
 	
 	velocity.y = velocity.y - GRAVITY
 	
@@ -30,25 +30,4 @@ func _on_Area_body_entered(body):
 	get_tree().reload_current_scene()
 
 func _on_Goal_body_entered(body):
-	debug_mode = true
 	translation.x = -10
-
-# DEBUG MODE STUFF
-var mouse_sens = 0.3
-var camera_anglev=0
-
-func _input(event):         
-	if event is InputEventMouseMotion and debug_mode:
-		$InterpolatedCamera.rotate_y(deg2rad(-event.relative.x*mouse_sens))
-		var changev=-event.relative.y*mouse_sens
-		if camera_anglev+changev>-50 and camera_anglev+changev<50:
-			camera_anglev+=changev
-			$InterpolatedCamera.rotate_x(deg2rad(changev))
-	if Input.is_action_pressed("debug_up") and debug_mode:
-		$InterpolatedCamera.translation.z -= 1
-	if Input.is_action_pressed("debug_down") and debug_mode:
-		$InterpolatedCamera.translation.z += 1
-	if Input.is_action_pressed("debug_left") and debug_mode:
-		$InterpolatedCamera.translation.x -= 1
-	if Input.is_action_pressed("debug_right") and debug_mode:
-		$InterpolatedCamera.translation.x += 1
